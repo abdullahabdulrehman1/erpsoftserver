a259a68cb2119607af72f0f760c38063175add21
abdullahabdulrehman1 <abdullahabdurrehman72@gmail.com>
2025-02-15 02:07:04 +0500

    search functionality adding

--- routes/GRNReturnGeneral.js
+++ routes/GRNReturnGeneral.js
@@ -1,5 +1,5 @@
-import express from "express";
-import { isAuthenticated } from "../middlewares/auth.js";
+import express from 'express'
+import { isAuthenticated } from '../middlewares/auth.js'
 import {
   createReturnGRN,
   deleteGRNReturnById,
@@ -7,15 +7,15 @@ import {
   generateGRNReturnReport,
   getGRNReturnsByUserId,
   searchGRNReturnGeneral
-} from "../controllers/GRNReturnGeneral.js";
-const app = express();
-app.use(isAuthenticated);
-app.post("/create-return-grn", createReturnGRN);
-app.get("/get-grn-returns", getGRNReturnsByUserId);
-app.delete("/delete-grn-return-general", deleteGRNReturnById);
-app.put("/edit-grn-return-general", editGRNReturnById);
+} from '../controllers/GRNReturnGeneral.js'
+const app = express()
+app.use(isAuthenticated)
+app.post('/create-return-grn', createReturnGRN)
+app.get('/get-grn-returns', getGRNReturnsByUserId)
+app.delete('/delete-grn-return-general', deleteGRNReturnById)
+app.put('/edit-grn-return-general', editGRNReturnById)
 
-app.get("/generatePdfReport", generateGRNReturnReport);
-app.get("/searchGRNReturnGeneral", searchGRNReturnGeneral); // Add this line for search route
+app.get('/generatePdfReport', generateGRNReturnReport)
+app.get('/searchGRNReturnGeneral', searchGRNReturnGeneral) // Add this line for search route
 
-export default app;
+export default app
